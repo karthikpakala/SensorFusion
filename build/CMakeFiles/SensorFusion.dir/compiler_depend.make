ff04dd97b0d0c6430288fbea01b86afbf4d2ded4
pakalakarthik1@gmail.com <pakalakarthik1@gmail.com>
2024-05-24 11:28:04 -0400

    Updates to build.sh

--- build/CMakeFiles/SensorFusion.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for SensorFusion.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/SensorFusion.dir/src/KalmanFilter.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/SensorFusion.dir/src/KalmanFilter.cpp.o: \
-  /Users/karthikpakala/Projects/SensorFusion/src/KalmanFilter.cpp
